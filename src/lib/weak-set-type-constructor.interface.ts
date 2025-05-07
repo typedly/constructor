aa1149ec85acfcf5ce74152f7a75c11ae9fd3707
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-07 15:43:09 +0000

    refactor(`WeakSetTypeConstructor`): update params.

--- src/lib/weak-set-type-constructor.interface.ts
+++ src/lib/weak-set-type-constructor.interface.ts
@@ -2,5 +2,5 @@ export interface WeakSetTypeConstructor<
   Type extends object,
   SetType extends WeakSet<Type> = WeakSet<Type>
 > {
-  new (iterable?: Iterable<Type>): SetType;
+  new (values?: readonly Type[]): SetType;
 }
